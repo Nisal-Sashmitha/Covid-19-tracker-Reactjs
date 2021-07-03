bd3e6f66ce7c7e1e2296fce5c7c39affc12447b3
Nisal-Sashmitha <it20081034@my.sliit.lk>
2021-07-03 15:54:26 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,73 @@
-
+import React, {useState,useEffect} from 'react';
+import { FormControl,Select,MenuItem } from '@material-ui/core';
 import './App.css';
 
+//"https://disease.sh/v3/covid-19/countries"
+
 function App() {
+
+
+  const [Countries, setCountries] =useState(["sri lanka", "UK", "India"]);
+
+  const [country,setCountry] = useState("worldwide");
+
+
+  useEffect(() => {
+
+    const getCountiesData = async()=>{
+      await fetch ("https://disease.sh/v3/covid-19/countries").then((response)=> response.json()).then((data)=>{
+        const countries = data.map((country)=>(
+          {
+            name:country.country,
+            value:country.countryInfo.iso2,
+          }));
+          setCountries(countries);
+      });
+    };
+
+    getCountiesData();
+    
+  }, [Countries])
+
+  const onCountryChange = (event)=>{
+    const countryCode = event.target.value;
+    console.log("YOOOO>>>",countryCode);
+    setCountry(countryCode);
+
+  }
+
   return (
     <div className="App">
-      
+      <div className="app__header">
+         <h1>COVID-19 TRACKER</h1>
+
+       <FormControl className="app__dropdown">
+          <Select variant="outlined" value={country} onChange={onCountryChange}>
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+              {
+                Countries.map(country=>(
+                  <MenuItem value={country.name}>{country.name}</MenuItem>
+                ))
+              }
+
+          {/*<MenuItem value="worldwide">Worldwide</MenuItem>
+            <MenuItem value="worldwide">Option two</MenuItem>
+            <MenuItem value="worldwide">Option three</MenuItem>
+            <MenuItem value="worldwide">Option four</MenuItem>*/}
+          </Select>  
+
+        </FormControl> 
+      </div>
+
+      {/*Header */}
+      {/*Title + select input dropdown field */}
+      {/* InfoBoxes */}
+      {/*InfoBoxes*/}
+      {/*InfoBoxes*/}
+
+      {/*Table*/}
+      {/*Graph*/}
+      {/*Map*/}
     </div>
   );
 }
